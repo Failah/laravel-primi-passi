2c922bc392561100c73db70a9293bc14b73cca00
ValerioScharizza <valerio.schiarizza@live.it>
2022-10-27 14:40:15 +0200

    created 2 new routes

--- laravel-primi-passi/resources/views/home.blade.php
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -1,9 +1,30 @@
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+    }
+
+    #navbar {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        column-gap: 20px;
+        background-color: bisque;
+    }
+
+    h2 {
+        color: red;
+    }
+</style>
+
 @php
 
-    $links = ['Home', 'Credits', 'Infos'];
+    $links = ['Home', 'Infos', 'Credits'];
 
 @endphp
 
-@foreach ($links as $item)
-    <h2> {{ $item }} </h2>
-@endforeach
+<div id="navbar">
+    @foreach ($links as $item)
+        <h2> {{ $item }} </h2>
+    @endforeach
+</div>
--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/infos', function () {
+    return view('infos');
+});
+
+Route::get('/credits', function () {
+    return view('credits');
 });
